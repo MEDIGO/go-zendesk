3582b0af068fb96e6ed13c92da96c3138c6d2255
Alex Giannakakos <alexgiannakakos@gmail.com>
2017-01-09 15:19:26 +0100

    Fix failing Ticket tests

--- test/integration/fixture.go
+++ test/integration/fixture.go
@@ -35,6 +35,7 @@ func RandTicket(client zendesk.Client, user *zendesk.User) (*zendesk.Ticket, err
 		Subject:     zendesk.String("My printer is on fire!"),
 		Description: zendesk.String("The smoke is very colorful."),
 		RequesterID: user.ID,
+		Type:	     zendesk.String("problem"),
 		Tags:        []string{"test"},
 	}
 
