78c2b50db2814f6bda3d926c81ee5409cc1f036f
Alex Giannakakos <alexgiannakakos@gmail.com>
2017-10-02 11:51:11 +0200

    *tp4242 - Implement Identity API

---
+++ zendesk/user_identity.go
@@ -0,0 +1,68 @@
+package zendesk
+
+import (
+	"fmt"
+	"time"
+)
+
+// UserIdentity represents a Zendesk user identity.
+//
+// Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/user_identities
+type UserIdentity struct {
+	ID                 *int64     `json:"id,omitempty"`
+	URL                *string    `json:"url,omitempty"`
+	UserID             *int64     `json:"user_id,omitempty"`
+	Type               *string    `json:"type,omitempty"`
+	Value              *string    `json:"value,omitempty"`
+	Verified           *bool      `json:"verified,omitempty"`
+	Primary            *bool      `json:"primary,omitempty"`
+	CreatedAt          *time.Time `json:"created_at,omitempty"`
+	UpdatedAt          *time.Time `json:"updated_at,omitempty"`
+	UndeliverableCount *int64     `json:"undeliverable_count,omitempty"`
+	DeliverableState   *string    `json:"deliverable_state,omitempty"`
+}
+
+// ListIdentities lists all user identities.
+//
+// Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/user_identities#list-identities
+func (c *client) ListIdentities(userID int64) ([]UserIdentity, error) {
+	out := new(APIPayload)
+	err := c.get(fmt.Sprintf("/api/v2/users/%d/identities.json", userID), out)
+	return out.Identities, err
+}
+
+// ShowIdentity fetches a user identity by its ID and user ID.
+//
+// Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/user_identities#show-identity
+func (c *client) ShowIdentity(userID, id int64) (*UserIdentity, error) {
+	out := new(APIPayload)
+	err := c.get(fmt.Sprintf("/api/v2/users/%d/identities/%d.json", userID, id), out)
+	return out.Identity, err
+}
+
+// CreateIdentity creates a user identity.
+//
+// Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/user_identities#create-identity
+func (c *client) CreateIdentity(userID int64, identity *UserIdentity) (*UserIdentity, error) {
+	in := &APIPayload{Identity: identity}
+	out := new(APIPayload)
+	err := c.post(fmt.Sprintf("/api/v2/users/%d/identities.json", userID), in, out)
+	return out.Identity, err
+}
+
+// UpdateIdentity updates the value and verified status of a user identity.
+//
+// Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/user_identities#update-identity
+func (c *client) UpdateIdentity(userID, id int64, identity *UserIdentity) (*UserIdentity, error) {
+	in := &APIPayload{Identity: identity}
+	out := new(APIPayload)
+	err := c.put(fmt.Sprintf("/api/v2/users/%d/identities/%d.json", userID, id), in, out)
+	return out.Identity, err
+}
+
+// DeleteIdentity deletes a user identity.
+//
+// Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/user_identities#delete-identity
+func (c *client) DeleteIdentity(userID, id int64) error {
+	return c.delete(fmt.Sprintf("/api/v2/users/%d/identities/%d.json", userID, id), nil)
+}
---
+++ zendesk/user_identity_test.go
@@ -0,0 +1,77 @@
+package zendesk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIdentityCRUD(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test in short mode.")
+	}
+
+	client, err := NewEnvClient()
+	require.NoError(t, err)
+
+	// create user
+	newUser := User{
+		Name:  String(randString(16)),
+		Email: String(randString(16) + "@example.com"),
+	}
+
+	user, err := client.CreateUser(&newUser)
+	require.NoError(t, err)
+	require.NotNil(t, user.ID)
+
+	// get user
+	_, err = client.ShowUser(*user.ID)
+	require.NoError(t, err)
+
+	// get user identity from list
+	found, err := client.ListIdentities(*user.ID)
+	require.NoError(t, err)
+	require.Equal(t, len(found), 1)
+	require.Equal(t, *found[0].Value, *user.Email)
+
+	// update user identity
+	newIdentity := UserIdentity{
+		Value: String(randString(16) + "@example.com"),
+	}
+
+	_, err = client.UpdateIdentity(*user.ID, *found[0].ID, &newIdentity)
+	require.NoError(t, err)
+
+	// get user identity by id
+	fetched, err := client.ShowIdentity(*user.ID, *found[0].ID)
+	require.NoError(t, err)
+	require.NotNil(t, fetched)
+	require.Equal(t, fetched.Value, newIdentity.Value)
+
+	// make sure the primary email has changed in user
+	fetchedUser, err := client.ShowUser(*user.ID)
+	require.NoError(t, err)
+	require.NotNil(t, fetchedUser)
+	require.Equal(t, *fetchedUser.Email, *newIdentity.Value)
+
+	// create user identity (secondary email)
+	secondaryIdentity := UserIdentity{
+		Type:  String("email"),
+		Value: String(randString(16) + "@example.com"),
+	}
+
+	created, err := client.CreateIdentity(*user.ID, &secondaryIdentity)
+	require.NoError(t, err)
+
+	found, err = client.ListIdentities(*user.ID)
+	require.NoError(t, err)
+	require.Equal(t, len(found), 2)
+
+	// delete user identity
+	err = client.DeleteIdentity(*user.ID, *created.ID)
+	require.NoError(t, err)
+
+	found, err = client.ListIdentities(*user.ID)
+	require.NoError(t, err)
+	require.Equal(t, len(found), 1)
+}
