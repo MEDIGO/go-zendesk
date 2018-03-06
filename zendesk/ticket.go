e22c916d9e88af826291604b571f353db0a90cda
Zsolt Szabo <debeloksanyi@gmail.com>
2018-03-06 13:31:54 +0100

    Add followup source field to ticket

--- zendesk/ticket.go
+++ zendesk/ticket.go
@@ -11,35 +11,36 @@ import (
 //
 // Zendesk Core API docs: https://developer.zendesk.com/rest_api/docs/core/tickets
 type Ticket struct {
-	ID              *int64         `json:"id,omitempty"`
-	URL             *string        `json:"url,omitempty"`
-	ExternalID      *string        `json:"external_id,omitempty"`
-	Type            *string        `json:"type,omitempty"`
-	Subject         *string        `json:"subject,omitempty"`
-	RawSubject      *string        `json:"raw_subject,omitempty"`
-	Description     *string        `json:"description,omitempty"`
-	Comment         *TicketComment `json:"comment,omitempty"`
-	Priority        *string        `json:"priority,omitempty"`
-	Status          *string        `json:"status,omitempty"`
-	Recipient       *string        `json:"recipient,omitempty"`
-	RequesterID     *int64         `json:"requester_id,omitempty"`
-	Requester       *User          `json:"requester,omitempty"`
-	SubmitterID     *int64         `json:"submitter_id,omitempty"`
-	AssigneeID      *int64         `json:"assignee_id,omitempty"`
-	OrganizationID  *int64         `json:"organization_id,omitempty"`
-	GroupID         *int64         `json:"group_id,omitempty"`
-	CollaboratorIDs []int64        `json:"collaborator_ids,omitempty"`
-	ForumTopicID    *int64         `json:"forum_topic_id,omitempty"`
-	ProblemID       *int64         `json:"problem_id,omitempty"`
-	HasIncidents    *bool          `json:"has_incidents,omitempty"`
-	DueAt           *time.Time     `json:"due_at,omitempty"`
-	Tags            []string       `json:"tags,omitempty"`
-	Via             *Via           `json:"via,omitempty"`
-	CreatedAt       *time.Time     `json:"created_at,omitempty"`
-	UpdatedAt       *time.Time     `json:"updated_at,omitempty"`
-	CustomFields    []CustomField  `json:"custom_fields,omitempty"`
-	BrandID         *int64         `json:"brand_id,omitempty"`
-	TicketFormID    *int64         `json:"ticket_form_id,omitempty"`
+	ID               *int64         `json:"id,omitempty"`
+	URL              *string        `json:"url,omitempty"`
+	ExternalID       *string        `json:"external_id,omitempty"`
+	Type             *string        `json:"type,omitempty"`
+	Subject          *string        `json:"subject,omitempty"`
+	RawSubject       *string        `json:"raw_subject,omitempty"`
+	Description      *string        `json:"description,omitempty"`
+	Comment          *TicketComment `json:"comment,omitempty"`
+	Priority         *string        `json:"priority,omitempty"`
+	Status           *string        `json:"status,omitempty"`
+	Recipient        *string        `json:"recipient,omitempty"`
+	RequesterID      *int64         `json:"requester_id,omitempty"`
+	Requester        *User          `json:"requester,omitempty"`
+	SubmitterID      *int64         `json:"submitter_id,omitempty"`
+	AssigneeID       *int64         `json:"assignee_id,omitempty"`
+	OrganizationID   *int64         `json:"organization_id,omitempty"`
+	GroupID          *int64         `json:"group_id,omitempty"`
+	CollaboratorIDs  []int64        `json:"collaborator_ids,omitempty"`
+	ForumTopicID     *int64         `json:"forum_topic_id,omitempty"`
+	ProblemID        *int64         `json:"problem_id,omitempty"`
+	HasIncidents     *bool          `json:"has_incidents,omitempty"`
+	DueAt            *time.Time     `json:"due_at,omitempty"`
+	Tags             []string       `json:"tags,omitempty"`
+	Via              *Via           `json:"via,omitempty"`
+	CreatedAt        *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt        *time.Time     `json:"updated_at,omitempty"`
+	CustomFields     []CustomField  `json:"custom_fields,omitempty"`
+	BrandID          *int64         `json:"brand_id,omitempty"`
+	TicketFormID     *int64         `json:"ticket_form_id,omitempty"`
+	FollowupSourceID *int64         `json:"via_followup_source_id,omitempty"`
 
 	AdditionalTags []string `json:"additional_tags,omitempty"`
 	RemoveTags     []string `json:"remove_tags,omitempty"`
