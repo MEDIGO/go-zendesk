28d16fa67e3f0faeb6e9acf40a4464bea44863da
André Cruz <andre@cabine.org>
2019-06-26 11:03:03 +0100

    New function to list comments that exposes options (#78)
    * New function to list comments that exposes options
    https://developer.zendesk.com/rest_api/docs/support/ticket_comments#list-email-ccs-for-a-comment
    
    * Added comment with link to API docs

--- zendesk/audits.go
+++ zendesk/audits.go
@@ -1,5 +1,39 @@
 package zendesk
 
+// The via object of a ticket audit or audit event tells you how or why the audit or event was created
+//
+// Zendesk Via API docs: https://developer.zendesk.com/rest_api/docs/support/ticket_audits#the-via-object
 type Via struct {
-	Channel *string
+	Channel *string `json:"channel,omitempty"`
+	Source  *Source `json:"source,omitempty"`
+}
+
+type Source struct {
+	To   *SourceInfo `json:"to,omitempty"`
+	From *SourceInfo `json:"from,omitempty"`
+	Rel  *string     `json:"rel,omitempty"`
+}
+
+type SourceInfo struct {
+	Address                          *string       `json:"address,omitempty"`
+	Deleted                          *bool         `json:"deleted,omitempty"`
+	FacebookID                       *string       `json:"facebook_id,omitempty"`
+	FormattedPhone                   *string       `json:"formatted_phone,omitempty"`
+	ID                               *int          `json:"id,omitempty"`
+	Name                             *string       `json:"name,omitempty"`
+	OriginalRecipients               []string      `json:"original_recipients,omitempty"`
+	EmailCCs                         []interface{} `json:"email_ccs,omitempty"`
+	Phone                            *string       `json:"phone,omitempty"`
+	ProfileURL                       *string       `json:"profile_url,omitempty"`
+	RegisteredIntegrationServiceName *string       `json:"registered_integration_service_name,omitempty"`
+	RevisionID                       *int          `json:"revision_id,omitempty"`
+	ServiceInfo                      *string       `json:"service_info,omitempty"`
+	Subject                          *string       `json:"subject,omitempty"`
+	SupportsChannelback              *bool         `json:"supports_channelback,omitempty"`
+	SupportsClickthrough             *bool         `json:"supports_clickthrough,omitempty"`
+	TicketID                         *int          `json:"ticket_id,omitempty"`
+	Title                            *string       `json:"title,omitempty"`
+	TopicID                          *int          `json:"topic_id,omitempty"`
+	TopicName                        *string       `json:"topic_name,omitempty"`
+	Username                         *string       `json:"username,omitempty"`
 }
